7c46033a6501aa956461dd3cb27dc7e3cad0117d
Sethen Maleno <sethenm@gmail.com>
2016-04-26 16:41:13 -0400

    Few more style tweaks

--- examples/style.js
+++ examples/style.js
@@ -14,7 +14,10 @@ ReactDOM.render(<BrandButton>Primary Button</BrandButton>, document.getElementBy
 ReactDOM.render(<FemaleButton>Primary Button</FemaleButton>, document.getElementById('female-button'));
 ReactDOM.render(<MaleButton>Primary Button</MaleButton>, document.getElementById('male-button'));
 ReactDOM.render(<PrimaryButton>Primary Button</PrimaryButton>, document.getElementById('primary-button'));
+ReactDOM.render(<PrimaryButton disabled>Primary Disabled Button</PrimaryButton>, document.getElementById('primary-disabled-button'));
 ReactDOM.render(<PrimaryNeutralButton>Primary Neutral Button</PrimaryNeutralButton>, document.getElementById('primary-neutral-button'));
+ReactDOM.render(<PrimaryNeutralButton disabled>Primary Neutral Disabled Button</PrimaryNeutralButton>, document.getElementById('primary-neutral-disabled-button'));
 ReactDOM.render(<PrimaryPositiveButton>Primary Positive Button</PrimaryPositiveButton>, document.getElementById('primary-positive-button'));
+ReactDOM.render(<PrimaryPositiveButton disabled>Primary Positive Disabled Button</PrimaryPositiveButton>, document.getElementById('primary-positive-disabled-button'));
 ReactDOM.render(<SecondaryButton>Secondary Button</SecondaryButton>, document.getElementById('secondary-button'));
 ReactDOM.render(<SecondaryPositiveButton>Secondary Positive Button</SecondaryPositiveButton>, document.getElementById('secondary-positive-button'));
