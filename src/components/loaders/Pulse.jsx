d6fcff39da6cb5735856f90dfe99567de11b3bae
JonathanBruce <jbruce232@gmail.com>
2017-02-21 16:19:39 -0500

    updated pulse loader to include a prompt parameter refs #3963

--- src/components/loaders/Pulse.jsx
+++ src/components/loaders/Pulse.jsx
@@ -2,18 +2,29 @@
 
 class PulseLoader extends React.Component {
 	static propTypes = {
+		prompt: React.PropTypes.string,
 		size: React.PropTypes.number
 	};
 
 	render() {
-		const { size } = this.props;
+		const { prompt, size } = this.props;
 		const pulseStyles = {
 			height: size,
 			width: size
 		};
 
 		return (
-			<div className='sk-spinner sk-spinner-pulse' style={ pulseStyles }></div>
+			<div className='orch-pulse-loading'>
+				{
+					prompt
+					&& (
+						<div className='loading-text'>
+							{ prompt }
+						</div>
+					)
+				}
+				<div className='sk-spinner sk-spinner-pulse' style={ pulseStyles }></div>
+			</div>
 		);
 	}
 }
